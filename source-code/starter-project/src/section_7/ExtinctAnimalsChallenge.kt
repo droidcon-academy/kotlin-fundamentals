d2498c4bbdb8c2c9193e53e30767000f36135a3c
Ahmed Atef <atef.ahmed1341@gmail.com>
2025-04-29 08:11:18 +0300

    add starter code for extinct animals challenge

---
+++ source-code/starter-project/src/section_7/ExtinctAnimalsChallenge.kt
@@ -0,0 +1,15 @@
+package section_7
+
+fun main() {
+    val animals = arrayOf(
+        "African Forest Elephant",
+        "Amur Leopard",
+        "Axolotl",
+        "Javan Rhino",
+        "Mountain Gorilla",
+        "Sunda Island Tiger",
+        "Tapanuli Orangutan"
+    )
+    for (element in animals)
+        println(element)
+}
