0e753841db8c4d577a02716a93348d00662d3fa9
Ahmed Atef <atef.ahmed1341@gmail.com>
2025-04-30 01:00:38 +0300

    add starter code for supermarket challenge

---
+++ source-code/starter-project/src/section_7/SupermarketChallenge.kt
@@ -0,0 +1,21 @@
+package section_7
+
+fun main() {
+    val cairoCustomers = mutableSetOf<String>()
+    cairoCustomers.add("Kotlin")
+    cairoCustomers.add("Lollipop")
+
+    val newDelhiCustomers = mutableSetOf<String>()
+    newDelhiCustomers.add("Java")
+    newDelhiCustomers.add("Kotlin")
+
+    val allCustomers = cairoCustomers union newDelhiCustomers
+    println(allCustomers)
+
+    val globalCustomers = cairoCustomers intersect newDelhiCustomers
+    println(globalCustomers)
+
+    val cairoLocalCustomers = cairoCustomers subtract newDelhiCustomers
+    println(cairoLocalCustomers)
+
+}
